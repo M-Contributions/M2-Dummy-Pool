83a00c5aa3946df747796b6927b0ad4c7ebddb54
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2020-04-09 20:16:51 +0200

    Add ftp command

---
+++ Console/Command/FtpConnector.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Command Class
+ * @category    Ticaje
+ * @package     Ticaje_Dummy
+ * @author      Hector Luis Barrientos <ticaje@filetea.me>
+ */
+
+namespace Ticaje\Dummy\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Ticaje\Connector\Gateway\Client\Ftp as FtpClient;
+use Ticaje\Connector\Gateway\Provider\Credentials as CredentialsManager;
+
+/**
+ * Class Instantiate
+ * @package Ticaje\Connector\Command
+ */
+class FtpConnector extends Command
+{
+    protected $client;
+
+    protected $credentials;
+
+    public function __construct(
+        FtpClient $client,
+        CredentialsManager $credentials,
+        string $name = null
+    ) {
+        $this->client = $client;
+        $this->credentials = $credentials;
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+        $this->setName("ticaje:test:ftp:connector");
+        $this->setDescription("Test Connector Extension.");
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $credentials = [
+            'username' => 'sftpuser',
+            'password' => 'sftpuser',
+            'host' => 'localhost',
+            'port' => 22,
+
+        ];
+        $result = null;
+        $this->credentials->set($credentials);
+        $connected = $this->client->connect($this->credentials);
+        if ($connected) {
+            $result = $this->client->download('comeco/sftp/Descriptions.csv');
+        }
+        $output->write($result, true);
+    }
+}
